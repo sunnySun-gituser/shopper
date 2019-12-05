e8d619a336dcf0a51484bab6768e7eaf4c7ebf62
sunnySun-gituser <57857509+sunnySun-gituser@users.noreply.github.com>
2019-12-05 12:31:15 -0800

    Updates

---
+++ precache-manifest.3ab7b8d7f0f2af8a0476fdc667e1f8c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02af970316a705d13bfbb2539225fcc",
+    "url": "/shopper/index.html"
+  },
+  {
+    "revision": "052775750a26793b65a9",
+    "url": "/shopper/static/css/main.2d95410f.chunk.css"
+  },
+  {
+    "revision": "d4430cd3b0ee086eabae",
+    "url": "/shopper/static/js/2.75b3089a.chunk.js"
+  },
+  {
+    "revision": "052775750a26793b65a9",
+    "url": "/shopper/static/js/main.eb25a455.chunk.js"
+  },
+  {
+    "revision": "40ab8780780289df2e81",
+    "url": "/shopper/static/js/runtime-main.d9159bab.js"
+  }
+]);
